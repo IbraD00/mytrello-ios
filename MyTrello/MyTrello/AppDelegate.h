07b17c08fe377408b86182f03aa6f15b31836887
Ibra <ibrahima.dansokho@outlook.com>
2016-07-11 14:15:11 +0200

    init

---
+++ MyTrello/MyTrello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTrello
+//
+//  Created by Ibrahima Dansokho on 11/07/2016.
+//  Copyright © 2016 Ibrahima Dansokho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTrello/MyTrello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTrello
+//
+//  Created by Ibrahima Dansokho on 11/07/2016.
+//  Copyright © 2016 Ibrahima Dansokho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
